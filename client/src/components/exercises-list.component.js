6156152e773c42a57ecf796878e743e0efcc8f3d
Michael <michaell7ii@hotmail.com>
2021-04-18 20:54:43 -0600

    Rearrange directory

--- client/src/components/exercises-list.component.js
+++ client/src/components/exercises-list.component.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://fitboards.herokuapp.com";
 
 const Exercise = (props) => (
   <tr>
